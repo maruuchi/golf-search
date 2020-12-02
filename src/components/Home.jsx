2b3a93c49761277fb16a0a1f01a8f64e91c1aa65
soma-maruuchi <mwa60346@gmail.com>
2020-12-02 12:54:34 +0900

    ローディングの作成

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,7 @@ import addDays from "date-fns/addDays";
 import axios from "axios";
 import format from "date-fns/format";
 import Result from "./Result";
+import Loading from './Loading';
 
 const Today = new Date();
 registerLocale("ja", ja);
@@ -20,12 +21,14 @@ class Home extends React.Component {
     duration: "90",
     planCount: 0, 
     plans: null, 
-    error: null
+    error: false, 
+    loading: false
   };
 
   onFormSubmit = async (event) => {
     try {
       event.preventDefault();
+      this.setState({ loading: true });
 
       const response = await axios.get(
         "https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses",
@@ -42,6 +45,7 @@ class Home extends React.Component {
         planCount: response.data.count,
         plans: response.data.plans
       });
+      this.setState({ loading: false });
     } catch (e) {
       this.setState({ error: e })
     }
@@ -114,6 +118,9 @@ class Home extends React.Component {
               </button>
             </div>
           </form>
+
+          <Loading loading={this.state.loading} />
+          
           <Result 
           plans={this.state.plans} 
           planCount={this.state.planCount} 
